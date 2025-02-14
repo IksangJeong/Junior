7c2251e81443c587708709ee85be980391a05c09
정익상 <wjddlrtkd0110@naver.com>
2025-02-14 20:55:29 +0900

    트리 자료구조

---
+++ Python/data_structures/list_practice_66.py
@@ -0,0 +1,43 @@
+"""
+리스트 자료구조 연습 #66
+"""
+
+class CustomList_66:
+    """커스텀 리스트 #66"""
+    def __init__(self):
+        self.data = []
+        self.size = 0
+
+    def append(self, value):
+        self.data.append(value)
+        self.size += 1
+
+    def insert_at(self, index, value):
+        if 0 <= index <= self.size:
+            self.data.insert(index, value)
+            self.size += 1
+
+    def remove_at(self, index):
+        if 0 <= index < self.size:
+            val = self.data.pop(index)
+            self.size -= 1
+            return val
+        return None
+
+    def find(self, value):
+        for i, v in enumerate(self.data):
+            if v == value:
+                return i
+        return -1
+
+    def __repr__(self):
+        return f"CustomList({self.data})"
+
+if __name__ == "__main__":
+    lst = CustomList_66()
+    for i in range(11):
+        lst.append(i * 5)
+    print(f"연습 #66: {lst}")
+    print(f"크기: {lst.size}")
+    lst.insert_at(2, 999)
+    print(f"삽입 후: {lst}")
