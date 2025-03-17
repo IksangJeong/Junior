e42f11b9a5b43aebe34b7814836de1e03d3dd76e
정익상 <wjddlrtkd0110@naver.com>
2025-03-17 20:28:23 +0900

    포인터 연습

---
+++ C/practice/exercise_120.c
@@ -0,0 +1,37 @@
+/* C 프로그래밍 연습 #120 */
+#include <stdio.h>
+
+/* 배열 관련 함수 #120 */
+void print_array_120(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int sum_array_120(int arr[], int n) {
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int max_element_120(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) max = arr[i];
+    }
+    return max;
+}
+
+int main() {
+    int arr[] = {360, 361, 362, 363};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("연습 #120\n");
+    printf("배열: ");
+    print_array_120(arr, n);
+    printf("합계: %d\n", sum_array_120(arr, n));
+    printf("최대: %d\n", max_element_120(arr, n));
+    return 0;
+}
